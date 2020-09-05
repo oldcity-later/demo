6be902dfd06515b677ac7641713dc651bdb724f3
oldcity-later <18002957476@163.com>
2020-09-05 22:54:18 +0800

    修改 增加index方法

--- src/main/java/com/study/demo/Test2.java
+++ src/main/java/com/study/demo/Test2.java
@@ -1,4 +1,23 @@
 package com.study.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+@RequestMapping("start/")
 public class Test2 {
+
+    public String index(Model m){
+        m.addAttribute("msg","hello Word");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2.class,args);
+    }
+
 }
